b60b674464b46532bae7232189c270f005441313
nasr89 <nasrnasr8915@gmail.com>
2024-02-01 14:58:44 +0200

    add controlles and routes

--- mycontacts-backend/routes/contactRoutes.js
+++ mycontacts-backend/routes/contactRoutes.js
@@ -1,24 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+} = require('../controllers/contactController')
 
-router.route('/').get((req, res) => {
-  res.status(200).json({ message: 'get all contacts' })
-})
+// router.route('/').get(getContacts)
 
-router.route('/').post((req, res) => {
-  res.status(200).json({ message: 'creat contact' })
-})
+// router.route('/').post(createContact)
 
-router.route('/:id').get((req, res) => {
-  res.status(200).json({ message: `get contact for ${req.params.id}` })
-})
+// router.route('/:id').get(getContact)
 
-router.route('/:id').put((req, res) => {
-  res.status(200).json({ message: `update contact for ${req.params.id}` })
-})
+// router.route('/:id').put(updateContact)
 
-router.route('/:id').delete((req, res) => {
-  res.status(200).json({ message: `delete contact for ${req.params.id}` })
-})
+// router.route('/:id').delete(deleteContact)
+
+// tertib aktar
+//--------------------------
+router.route('/').get(getContacts).post(createContact)
+
+router.route('/:id').get(getContact).put(updateContact).delete(deleteContact)
 
 module.exports = router
